c10980a373ed6de28d39d541c932ce8810c90374
Lunastela <lunastela128@gmail.com>
2024-03-14 08:59:52 -0400

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/me/Documents/sandbox
-# Build directory: /home/me/Documents/sandbox/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
